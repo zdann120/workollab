3ff381101bb77a92c6c7d4a349474c41895cbcf9
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-08-03 18:31:42 -0700

    add and update projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,39 @@ class ProjectsController < ApplicationController
     @project = Project.find params[:id]
   end
 
+  def new
+    @project = current_user.owned_projects.new
+  end
+
+  def create
+    @project = current_user.owned_projects.new(project_params)
+
+    if @project.save!
+      redirect_to @project, notice: 'Project created successfully!'
+    else
+      flash.now.alert = 'One or more errors prevented the project from being saved.'
+      render :new
+    end
+  end
+
   def edit
+    @project = Project.find params[:id]
+  end
+
+  def update
+    @project = Project.find params[:id]
+
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project updated successfully!'
+    else
+      flash.now.alert = 'One or more errors prevented this project from being updated. Please try again.'
+      render :edit
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :start_date)
   end
 end
